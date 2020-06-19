a920e18b76700fb55ffb7c17957c8eda8df668f1
VJ-Duardo <olafstein501@gmail.com>
2020-06-19 18:34:04 +0200

    added client na

--- botardo.js
+++ botardo.js
@@ -20,7 +20,7 @@ const opts = {
       password: pass.password
     },
     channels: [
-        "duardo1", "fabzeef", "chachnaq", "griphthefrog", "dieziege", "kiansly", "teischEnte", "college_boi", "ALazyMeme", "xjoselito101", "okabar", "vashiiq", "unlukky13", "Feyzintil", "SrLuuL"
+        "duardo1", "fabzeef", "chachnaq", "griphthefrog", "dieziege", "kiansly", "teischEnte", "college_boi", "ALazyMeme", "xjoselito101", "okabar", "vashiiq", "unlukky13", "Feyzintil", "SrLuuL", "NaMaca"
     ]
 };
 //in the future channels will be managed in the db
