1ffa71a8b20eeb243b9413ef70be73debc36d13a
VJ-Duardo <olafstein501@gmail.com>
2020-11-23 19:45:21 +0100

    added previous and immediate hit to board

--- darts.js
+++ darts.js
@@ -33,9 +33,9 @@ class Ring{
 const rings = [
     new Ring(0, 3, 25, "PogChamp bullseye!"),
     new Ring(3, 8, 18, "SeemsGood Second ring, pretty good."),
-    new Ring(8, 16, 10, ":/ Are you sure you read the rules?"),
-    new Ring(16, 23, 5, "FailFish Are you afk?"),
-    new Ring(23, 30, 1, "NotLikeThis Embarrassing."),
+    new Ring(8, 16, 10, ":/ Are you sure you read the rules? Only third ring."),
+    new Ring(16, 23, 5, "FailFish Fourth ring, are you afk?"),
+    new Ring(23, 30, 1, "NotLikeThis Fifth ring. Embarrassing."),
     new Ring(30, Number.POSITIVE_INFINITY, 0, "BibleThump You missed the board and hit my beautiful wall instead.")
 ];
 
@@ -62,11 +62,13 @@ class Game {
             status: false,
             handle: null
         };
+        this.hits = [];
         this.currentPoint = {
             x: 0,
             y: 0
         };
         this.generateRandomPointAscii = this.generateRandomPointAscii.bind(this);
+        this.evaluateRound = this.evaluateRound.bind(this);
         this.sayFunc(this.channelObj.name, "/me Get ready...");
         let startRound = this.generateRandomPointAscii;
         setTimeout(function(){startRound();}, timeToNextRound);
@@ -74,24 +76,14 @@ class Game {
     
     async generateRandomPointAscii(){
         let _this = this;
-        function loadAndAddToCanvas(url, x, y){
-            return loadImage(url)
-                .then((image) => {
-                    _this.context.drawImage(image, x, y, pixelWidth, pixelHeight);
-                    return 1;
-                })
-                .catch((err) => {
-                    console.log(err+" An error occured! (image)");
-                    return -1;
-                });
-        }
-        await loadAndAddToCanvas(boardImagePath, 0, 0);
+        await loadAndAddToCanvas(boardImagePath, 0, 0, this.context);
+        this.addPreviousHits(this.hits);
         
         const radius = (pixelWidth/2) * Math.sqrt(Math.random());
         const angle = Math.random() * 2 * Math.PI;
         const x = pixelWidth/2 + radius * Math.cos(angle);
         const y = (pixelHeight/2 + radius * Math.sin(angle));
-        await loadAndAddToCanvas(handImagePath, x, y-(pixelHeight * yCorrection));
+        await loadAndAddToCanvas(handImagePath, x, y-(pixelHeight * yCorrection), this.context);
         this.currentPoint.x = x;
         this.currentPoint.y = y;
         this.sayFunc(this.channelObj.name, "/me Round " +this.round+ "/" +maxRounds+ " " +printField(this.context));
@@ -100,7 +92,23 @@ class Game {
         this.waitForInput.handle = setTimeout(function(){_this.evaluateRound("");}, secondsToInput*1000);
     }
     
-    evaluateRound(input){
+    addPreviousHits(hits){
+        const font = "8px Arial";
+        const align = "center";
+        const yTextCorrection = 3;
+
+        this.context.fillStyle = "black";
+        this.context.font = font;
+        this.context.textAlign = align;
+        
+        for(const hit of hits){
+            if (hit.x < 0 || hit.x > pixelWidth || hit.y < 0 || hit.y > pixelHeight)
+                continue;
+            this.context.fillText('x', hit.x, hit.y+yTextCorrection);
+        }
+    }
+    
+    async evaluateRound(input){
         this.waitForInput.stats = false;
         for (let chunk of input.split(" ")){
             let characters = chunk.split("");
@@ -124,14 +132,20 @@ class Game {
             }
         }
         
+        this.hits.push({x: this.currentPoint.x, y: this.currentPoint.y});
         let distanceFromMiddle = Math.sqrt(((pixelWidth/2 - this.currentPoint.x) ** 2) + ((pixelHeight/2 - this.currentPoint.y) ** 2));
         let ring = rings.find(elem => distanceFromMiddle >= elem.start && distanceFromMiddle <= elem.end);
         this.points += ring.points;
+        
+        await loadAndAddToCanvas(boardImagePath, 0, 0, this.context);
+        this.addPreviousHits([{x: this.currentPoint.x, y: this.currentPoint.y}]);
+        this.sayFunc(this.channelObj.name, "/me "+ printField(this.context));
+        
         this.sayFunc(this.channelObj.name, "/me " +ring.message + ring.getPointsString() + " Points overall: " +this.points);
-        this.round++;
         if (this.round === maxRounds){
             this.endGame();
         } else {
+            this.round++;
             let _this = this;
             this.sayFunc(this.channelObj.name, "/me Get ready for the next round...");
             setTimeout(function(){_this.generateRandomPointAscii();}, timeToNextRound);
@@ -164,6 +178,20 @@ module.exports = {
         }
     }
 };
+
+
+function loadAndAddToCanvas(url, x, y, context){
+    return loadImage(url)
+        .then((image) => {
+            context.drawImage(image, x, y, pixelWidth, pixelHeight);
+            return 1;
+        })
+        .catch((err) => {
+            console.log(err+" An error occured! (image)");
+            return -1;
+        });
+}
+
 
 function printField(context){
     let pixelData = context.getImageData(0, 0, pixelWidth, pixelHeight).data;
